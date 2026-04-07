a1280a577ef7143ec74b2444e078c9328de50498
Javier <106848262+javiiicz@users.noreply.github.com>
2026-04-07 12:05:10 -0400

    new react NExt.js

---
+++ react-admin/admin/src/app/destinations/page.tsx
@@ -0,0 +1,4 @@
+export default function DestinationPage() {
+    return (<>
+    </>)
+}
---
+++ react-admin/admin/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <div>
+            <h2 className="text-3xl font-bold">Welcome to the Travel Site Dashboard</h2>
+            <p>This is a simple Next.js page</p>
+        </div>
+    );
+}
